3c46b101559a5426b67770f0e4c82a6057c33834
Aryan Vora <voraaryan1@gmail.com>
2023-12-19 12:51:18 -0800

    redeploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import "./globals.css";
 import App from "./App.jsx";
 import "@aws-amplify/ui-react/styles.css";
 import {Amplify} from "aws-amplify";
-import config from "./aws-exports";
+import awsconfig from "./aws-exports";
 import {ThemeProvider} from "@aws-amplify/ui-react";
 
-Amplify.configure(config);
+Amplify.configure(awsconfig);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider>
