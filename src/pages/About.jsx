cbc01b31c8ec72f41f9cfe550ca9b6679fa4557d
Aryan Vora <voraaryan1@gmail.com>
2023-12-18 23:44:35 -0800

    test

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h1>About Page Content</h1>
+      <a href="/">Home</a>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Home Page Content</h1>
+      <a href="/about">About</a>
+    </div>
+  );
+}
+
+export default Home;
